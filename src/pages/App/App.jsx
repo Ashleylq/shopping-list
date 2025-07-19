738fc261f11377314eb5450f409f69d0eb739705
Ashleylq <ironmanashley@gmail.com>
2025-07-19 15:30:13 +0000

    Add shop page

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,11 +1,15 @@
 import './App.module.css'
+import { Shop } from './Shop/Shop.jsx'
 
 export function App(){
     return(
-        <nav>
-            <h1>ElectroStop</h1>
-            <p>Shop!</p>
-            <p>Your Cart</p>
-        </nav>
+        <>
+            <nav>
+                <h1>ElectroStop</h1>
+                <p>Shop!</p>
+                <p>Your Cart</p>
+            </nav>
+            <Shop />
+        </>
     )
 }
--- src/pages/App/Shop/Shop.jsx
+++ src/pages/App/Shop/Shop.jsx
@@ -1,1 +1,63 @@
-import './Shop.module.css'
+import styles from './Shop.module.css'
+import { useState, useEffect } from 'react'
+
+function ItemCard({name, img}){
+    return(
+        <div className={styles.itemCard}>
+            <img src={img}
+            height="200px"/>
+            <p>{name}</p>
+        </div>
+    )
+}
+
+export function Shop(){
+    const [shopItems, setShopItems] = useState([]);
+    const [err, setErr] = useState('');
+    useEffect(() => {
+        async function fetchItems(){
+        try {
+            const res = await fetch("https://fakestoreapi.com/products/category/electronics");
+            if(!res.ok){
+                setErr("Hmm.. there seems to be an error, please try again later.");
+                return;
+            }
+            const items = await res.json();
+            let arr = [];
+            for(let item of items){
+                arr.push({
+                    id : crypto.randomUUID(),
+                    name : item.title,
+                    description : item.description,
+                    img : item.image
+                })
+            };
+            setShopItems(arr);
+            setErr("");
+        } catch(error){
+            setErr("UHHHH... WELP ITS AN ERROR")
+}}
+            fetchItems();
+    }, []);
+    /*useEffect(()=>{
+        alert(shopItems[0]);
+    }, [shopItems])*/
+    /*return(
+        <div>
+            {shopItems.map((item, i) => {
+                    return (<ItemCard key={i} name={item.title} img={item.image} />)
+                    return (<p>Hi</p>)
+             })}
+             Hello
+        </div>
+    )*/
+    if(shopItems.length === 0){
+        return <p>Loading...</p>
+    }
+    return(
+        <div className={styles.itemContainer}>
+            {shopItems.map((item) => {
+                return <ItemCard name={item.name} img={item.img}/>
+            })}
+        </div>);
+}
